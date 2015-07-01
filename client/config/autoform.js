b531a038acdad8d84c804038eb0ac5f068846f46
Eko Julianto Salim <ekojsalim@gmail.com>
2015-07-01 23:13:11 +0700

    Many fixes and implementatins aka I forgot to commit AGAIN

--- client/config/autoform.js
+++ client/config/autoform.js
@@ -1,1 +1,26 @@
 AutoForm.setDefaultTemplate("materialize");
+
+var postHooks = {
+  before: {
+    insert: function(doc) {
+        doc.ownerId = Meteor.userId();
+        doc.hits = 0;
+        doc.options = doc.options.map(function(item) {
+          return {
+            optionName: item.optionName,
+            votes: 0
+          };
+        });
+      return doc;
+    }
+  },
+  after: {
+	insert: function() {
+		Router.go("dashboard");
+		Materialize.toast("Poll Submitted!", 4000);
+	}
+  }
+};
+
+AutoForm.addHooks("newPollForm", postHooks);
+
