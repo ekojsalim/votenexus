6e73ba8c0c058e83d78ea322b4d75f431635312b
Eko Julianto Salim <ekojsalim@gmail.com>
2015-07-03 00:07:59 +0700

    Implement API

---
+++ server/json-config.js
@@ -0,0 +1,7 @@
+JsonRoutes.setResponseHeaders({
+  "Cache-Control": "no-store",
+  "Pragma": "no-cache",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
+});
